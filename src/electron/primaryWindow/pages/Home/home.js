4849f48dfbce2366a2009df4cab43ae1489c519e
Somjintana Korbut <nuisomjin@gmail.com>
2021-03-15 13:52:33 +0700

    CE-328 Implement file table header

--- src/electron/primaryWindow/pages/Home/home.js
+++ src/electron/primaryWindow/pages/Home/home.js
@@ -2,7 +2,17 @@ import Tab from './components/Tab'
 
 export default {
   name: 'Home',
+  data () {
+    return {
+      files: []
+    }
+  },
   components: {
     Tab
+  },
+  computed: {
+    isPreparedTab () {
+      return true // TODO: fix this
+    }
   }
 }
