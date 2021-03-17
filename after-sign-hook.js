d1608858b996ada7cdd0a97424b4092d4e188d83
Somjintana Korbut <nuisomjin@gmail.com>
2021-03-17 14:55:20 +0700

    CE-330 Setup notarized code for MacOS

--- after-sign-hook.js
+++ after-sign-hook.js
@@ -1,31 +1,30 @@
 const fs = require('fs')
 const path = require('path')
-let electron_notarize = require('electron-notarize')
-const { build: { appId } } = require('./package.json')
+let electronNotarize = require('electron-notarize')
+const dotenv = require('dotenv')
+dotenv.config()
 
 module.exports = async function (params) {
-    // Only notarize the app on Mac OS only.
-    if (process.platform !== 'darwin') {
-        return
-    }
-    // Same appId in electron-builder.
-    let appId = 'org.rfcx.ingest'
-    let ascProvider = 'TWEGNU8HTW'
-    let appPath = path.join(params.appOutDir, `${params.packager.appInfo.productFilename}.app`)
-    if (!fs.existsSync(appPath)) {
-        throw new Error(`Cannot find application at: ${appPath}`)
-    }
-    // console.log(`Notarizing ${appId} found at ${appPath}`)
-    try {
-        await electron_notarize.notarize({
-            appBundleId: appId,
-            appPath: appPath,
-            appleId: process.env.appleId,
-            appleIdPassword: process.env.appleIdPassword,
-            ascProvider: ascProvider
-        })
-    } catch (error) {
-        console.error('Error notarizing', error)
-    }
-    console.log(`Notarizing is finished`)
+  // Only notarize the app on Mac OS only.
+  if (process.platform !== 'darwin') {
+    return
+  }
+  // Same appId in electron-builder.
+  let appId = 'org.rfcx.ingest'
+  let appPath = path.join(params.appOutDir, `${params.packager.appInfo.productFilename}.app`)
+  if (!fs.existsSync(appPath)) {
+    throw new Error(`Cannot find application at: ${appPath}`)
+  }
+  // console.log(`Notarizing ${appId} found at ${appPath}`)
+  try {
+    await electronNotarize.notarize({
+      appBundleId: appId,
+      appPath: appPath,
+      appleApiKey: process.env.APPLE_API_KEY,
+      appleApiIssuer: process.env.APPLE_API_KEY_ISSUER_ID
+    })
+  } catch (error) {
+    console.error('Error notarizing', error)
+  }
+  console.log(`Notarizing is finished`)
 }
